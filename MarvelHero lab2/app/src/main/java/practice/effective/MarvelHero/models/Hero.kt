7d4b0f952e4e5b35f9a960ce622cd4fa9f3bffbe
bezrukiy <66155368+bezrukiy@users.noreply.github.com>
2022-12-01 16:36:17 +0600

    Delete MarvelHero lab2 directory

--- MarvelHero lab2/app/src/main/java/practice/effective/MarvelHero/models/Hero.kt
+++
@@ -1,9 +0,0 @@
-package practice.effective.MarvelHero.models
-
-data class Hero(
-    val id: String,
-    val name: String,
-    val description: String,
-    val imageUrl: String,
-//    val imageFormat: String,
-)

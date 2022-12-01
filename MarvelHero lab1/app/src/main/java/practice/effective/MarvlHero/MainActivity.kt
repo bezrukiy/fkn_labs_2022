373a303397fe40ab0e7af30e39f22ef6e7b5ef9c
bezrukiy <66155368+bezrukiy@users.noreply.github.com>
2022-12-01 17:00:39 +0600

    Fix lab1

---
+++ MarvelHero lab1/app/src/main/java/practice/effective/MarvlHero/MainActivity.kt
@@ -0,0 +1,35 @@
+package practice.effective.MarvlHero
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import practice.effective.MarvlHero.screens.ChoosingScreen
+import practice.effective.MarvlHero.ui.theme.ChooseYourHeroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ChooseYourHeroTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    ChoosingScreen()
+                }
+            }
+        }
+    }
+
+    @Preview
+    @Composable
+    fun Show() = ChoosingScreen()
+}
